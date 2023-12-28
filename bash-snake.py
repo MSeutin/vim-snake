a7f412e33a1929cdba70890651d324f861825386
Michael Seutin <frenchmike@gmail.com>
2023-12-28 02:59:33 -0800

    cleaned up my code and class

--- bash-snake.py
+++ bash-snake.py
@@ -1,5 +1,6 @@
 import curses
 import time
+import random
 
 class Snake:
     def __init__(self, stdscr):
@@ -14,17 +15,16 @@ class Snake:
         self.FRAME_DELAY = 1 / self.FRAME_RATE
         # not sure the color below is correct
         self.bright_red = curses.color_pair(2) | curses.A_BOLD
-        # call the initialize game function
         self.initialize_game()
-        # not sure how the game loop will work
-        # self.game_loop()
 
     def initialize_game(self):
-        # set up the game state
+        self.stdscr.clear()
+        self.max_y, self.max_x = self.stdscr.getmaxyx()
         self.cur_y, self.cur_x = self.max_y // 2, self.max_x // 2 # center initial snake pos
         self.stdscr.addstr(self.cur_y, self.cur_x, ' ', self.bright_red)  # Draw the cursor in orange
         self.food = [self.cur_y - 3, self.cur_x + 3] 
-        self.stdscr.addstr(self.food[0], self.food[1], '@', curses.color_pair(3) | curses.A_BOLD)  # Draw the food in green
+        self.stdscr.addstr(self.food[0], self.food[1], '@', curses.color_pair(3) | curses.A_BOLD) 
+        self.stdscr.refresh() # refresh screen to display the messages
 
     def input_handler(self, key):
         # Update cursor position based on key press
@@ -39,6 +39,7 @@ class Snake:
         elif key == 'q':  # Quit command
             return True
         return False
+             
 
     def update_game(self):
         # Update screen
@@ -54,7 +55,7 @@ class Snake:
         # Render food
         self.stdscr.addstr(self.food[0], self.food[1], '@', curses.color_pair(3) | curses.A_BOLD) 
 
-    def end_game(self):
+    def display_end_game_screen(self):
         # Display game over message
         self.stdscr.clear()
         game_over_msg = "G A M E  O V E R"
@@ -73,8 +74,19 @@ class Snake:
         # Wait for 'y' or 'n' key press
         while True:
             response = self.stdscr.getch()
-            if response in [ord('y'), ord('Y'), ord('n'), ord('N')]:
-                return response in [ord('y'), ord('Y')]
+            if response in [ord('y'), ord('Y')]:
+                self.end_game = False
+                self.score = 0
+                self.initialize_game() # restart game
+                break
+            elif response in [ord('n'), ord('N')]:
+                self.end_game = True
+                break
+
+    def display_fixed_elements(self):
+        self.display_logo()
+        self.display_score()
+        self.display_controls()
 
     def display_score(self):
         # Display score (top middle)
@@ -96,12 +108,6 @@ class Snake:
         logo = "S N A K E  G A M E"
         logo_x_pos = self.max_x // 2 - len(logo) // 2
         self.stdscr.addstr(self.max_y-2, logo_x_pos, logo, curses.A_BOLD)
-        
-    def set_end_game(self, end_game):
-        self.end_game = end_game
-        
-    def get_end_game(self):
-        return self.end_game
     
     def time_delay(self):
         time.sleep(self.FRAME_DELAY)
@@ -118,162 +124,38 @@ def main(stdscr):
     curses.curs_set(0)  # Hide cursor
     stdscr.nodelay(True)  # Make getkey() non-blocking
 
-
-
-    # Initialization Function
-    # def initialize_game(food, score, max_x, max_y):
-    #     # set up the game state
-    #     cur_y, cur_x = max_y // 2, max_x // 2 # center initial snake pos
-    #     stdscr.addstr(cur_y, cur_x, ' ', bright_red)  # Draw the cursor in orange
-    #     food = [cur_y - 3, cur_x + 3] 
-    #     stdscr.addstr(food[0], food[1], '@', curses.color_pair(3) | curses.A_BOLD)  # Draw the food in green
-    #     return cur_x, cur_y, food
-        
-
-    # # Input handling Function
-    # def input_handler(key, cur_x, cur_y, max_x, max_y):
-    #     # Update cursor position based on key press
-    #     if key == 'h' and cur_x > 0:
-    #         cur_x -= 1
-    #     elif key == 'l' and cur_x < max_x - 1:
-    #         cur_x += 1
-    #     elif key == 'j' and cur_y < max_y - 3:
-    #         cur_y += 1
-    #     elif key == 'k' and cur_y > 1:
-    #         cur_y -= 1
-    #     elif key == 'q':  # Quit command
-    #         return cur_x, cur_y, True
-    #     return cur_x, cur_y, False
-            
-
-    # # Update Function
-    # def update_game():
-    #     # Update screen
-    #     # move snake
-    #     # check for collisions
-    #     # check and update food
-    #     # return true if game over, else false
-    #     pass
-
-    # Render Function
-    # def render_game(cur_x, cur_y, food, score):
-    #     # Render snake
-    #     stdscr.addstr(cur_y, cur_x, ' ', bright_red)  # Draw the cursor in orange
-    #     # Render food
-    #     stdscr.addstr(food[0], food[1], '@', curses.color_pair(3) | curses.A_BOLD)  # Draw in green
-    #     # Render screen
-    #     # Render score
-    #     # Render controls
-
-    # # End game function 
-    # def end_game():
-    #     # Display game over message
-    #     stdscr.clear()
-    #     game_over_msg = "G A M E  O V E R"
-    #     play_again = "Play Another Game (y / n) ?"
-
-    #     # Ensure the message positions are within the screen bounds
-    #     max_y, max_x = stdscr.getmaxyx()
-    #     game_over_msg_y = max_y // 2 - 1
-    #     play_again_y = max_y // 2 + 1
-
-    #     # Print the messages
-    #     stdscr.addstr(game_over_msg_y, (max_x - len(game_over_msg)) // 2, game_over_msg, bright_red)
-    #     stdscr.addstr(play_again_y, (max_x - len(play_again)) // 2, play_again)
-    #     stdscr.refresh() # refresh screen to display the messages
-
-    #     # Wait for 'y' or 'n' key press
-    #     while True:
-    #         response = stdscr.getch()
-    #         if response in [ord('y'), ord('Y'), ord('n'), ord('N')]:
-    #             return response in [ord('y'), ord('Y')]
-
-    # def display_score(score, max_x):
-    #     # Display score (top middle)
-    #     score_text = f"Score: {score}"
-    #     score_x_pos = max_x // 2 - len(score_text) // 2
-    #     stdscr.addstr(0, score_x_pos, score_text, curses.color_pair(1))  # Display score
-    
-    # def display_controls(max_y):
-    #     controls = [('q', ':Quit'), ('h', ':Left'), ('j', ':Down'), ('k', ':Up'), ('l', ':Right')]
-    #     x = 1
-    #     for key, text in controls:
-    #         stdscr.addstr(max_y - 1, x, key, curses.color_pair(3) | curses.A_BOLD)
-    #         x += len(key)
-    #         stdscr.addstr(max_y - 1, x, text)
-    #         x += len(text) + 2
-    
-    # def display_logo(max_x, max_y):
-    #     # Display game name (bottom middle)
-    #     logo = "S N A K E  G A M E"
-    #     logo_x_pos = max_x // 2 - len(logo) // 2
-    #     stdscr.addstr(max_y-2, logo_x_pos, logo, curses.A_BOLD)
-    
     # Declare a snake object
     snake = Snake(stdscr)
 
-    # Outer Loop for the entire program
+    # Game Loop
     while True:
-        # Init
-        
-
-        # Call Initialize Game Function
-        # cur_x, cur_y, food = initialize_game(food, score, max_x, max_y)
-        
-        # Initialize game
-        snake.initialize_game()
-
-        # Inner Loop - Game Loop
-        while not snake.get_end_game():
-            # Clear the screen of all previously-printed characters
-            stdscr.clear()
-             
-            # Wait for next input
-            try:
-                key = stdscr.getkey()
-            except Exception:
-                key = '' 
-                
-            # Input Handling
-            quit_game = snake.input_handler(key)
+        stdscr.clear() # clear screen
             
-            # Update Game
-            snake.update_game()
+        # Wait for next input
+        try:
+            key = stdscr.getkey()
+        except Exception:
+            key = '' 
             
-            # Render Game
-            snake.render_game()
-
-            # Function calls
-            # cur_x, cur_y, quit_game = input_handler(key, cur_x, cur_y, max_x, max_y)
-            # update_game()
-            # render_game(cur_x, cur_y, food, score) # Render the game
+        # Input Handling
+        if snake.input_handler(key):
+            snake.display_end_game_screen()
             
-            # Display  Fixed Elements
-            snake.display_logo() # display logo
-            snake.display_score() # display score
-            snake.display_controls() # display controls
-            
-            # Display  Fixed Elements
-            # display_logo(max_x, max_y) # display logo
-            # display_score(score, max_x) # display score
-            # display_controls(max_y) # display controls        
-            
-            # Refresh the screen to update its contents
-            stdscr.refresh()
-            
-            # Wait 1/10 of a second before repeating
-            snake.time_delay()
-
-            # check if endgame
-            if quit_game:
-                play_again = snake.end_game()
-                if not play_again:
-                    snake.set_end_game(True)
-                break
-        if snake.get_end_game():
+        # check end game condition
+        if snake.end_game:
             break
-        else:
-            snake.initialize_game()
+        
+        # Update & Render Game
+        snake.update_game()
+        snake.render_game()
+        
+        # Display Elements
+        snake.display_fixed_elements()
+        
+        # Refresh screen & wait
+        stdscr.refresh()
+        snake.time_delay()
+        
 
 
 # Run the program
@@ -284,3 +166,18 @@ curses.wrapper(main)
 # test thoroughly on mac, linux, windows
 # unit testing (also make sure size of terminal can fit minimum)
 # package into executable and ship and sell online
+# Optimize display_end_game_screen Method:
+
+# After displaying the end game screen and getting the player's response, you could directly handle the restart or termination of the game within this method, rather than using additional checks in the main loop.
+# Improve Readability:
+
+# Adding comments to each method explaining its purpose can greatly improve the readability of your code, especially for others reading it or for yourself when you come back to it later.
+# Game Loop Optimization:
+
+# Clearing the screen (stdscr.clear()) and refreshing (stdscr.refresh()) in every iteration could be optimized. Ideally, you should clear and refresh the screen only when there's something new to display. This might require a bit of restructuring in how and when you update the game state and render it.
+# Handling Key Presses:
+
+# The try-except block for key presses in the main loop is good, but you might want to handle specific exceptions (like curses.error) instead of a general Exception to avoid catching unintended exceptions.
+# Update Method:
+
+# Flesh out the update_game method with the actual game logic. This is where you'll handle things like the movement of the snake, checking for collisions, and updating the game state.
